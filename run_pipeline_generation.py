dbf0012967fc05097d5f164744f66a835d083c01
Xirid <peter.k.albert@gmail.com>
2021-03-26 22:13:31 +0100

    changed pipeline to manual generation

--- run_pipeline_generation.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-generator = pipeline('text-generation', model='finetuned')
-generated_text = generator("This", max_length=200)[0]["generated_text"]
-print(generated_text)
--- texttocsv.py
+++ texttocsv.py
@@ -16,3 +16,5 @@ with open('validation.csv', mode='w', encoding='utf-8') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerow({'text': all_text})
+
+print("created train.csv and validation.csv files")
